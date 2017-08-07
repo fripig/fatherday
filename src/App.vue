714366540eb8de8bff2969a5d56ac3b403cd3579
fripig <fripig@gmail.com>
2017-08-08 02:36:37 +0800

    add  adsense

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <input type="text" v-model="fathername">
+    讓臉書創辦⼈⾺克祖克伯住每位⽗親⽗親節快樂！
+    輸⼊你想祝福的⽗親名字：<input type="text" v-model="fathername">
     <svgEditor :fathername="fathername"></svgEditor>
   </div>
 </template>
