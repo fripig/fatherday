2f5d0308d7a00f602318bc1d06c77cdab19ba97f
fripig <fripig@gmail.com>
2017-08-08 03:04:18 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,25 @@
     讓臉書創辦⼈⾺克祖克伯住每位⽗親⽗親節快樂！
     輸⼊你想祝福的⽗親名字：<input type="text" v-model="fathername">
     <svgEditor :fathername="fathername"></svgEditor>
+    <button @click="download">download</button>
   </div>
 </template>
 
 <script>
 
 import svgEditor  from './components/svgeditor'
+import {bus} from "./bus"
 
 export default {
   name: 'app',
   data(){
     return {
       fathername : ""
+    }
+  },
+  methods:{
+    download(){
+      bus.$emit('download', 1)
     }
   },
   components: {
