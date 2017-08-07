867890aab91312c40bad45b6465af7ab45cb7bf3
fripig <fripig@gmail.com>
2017-08-08 02:11:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="fathername">
+    <svgEditor :fathername="fathername"></svgEditor>
+  </div>
+</template>
+
+<script>
+
+import svgEditor  from './components/svgeditor'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      fathername : ""
+    }
+  },
+  components: {
+    svgEditor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
